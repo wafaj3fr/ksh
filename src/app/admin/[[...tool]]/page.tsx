b18b93cd8f6e852e86eb1f85b5f90020b1154bdf
Batman99SD <azozkrg1@gmail.com>
2025-04-28 13:38:32 +0300

    fixing the sanity studio

--- src/app/admin/[[...tool]]/page.tsx
+++ src/app/admin/[[...tool]]/page.tsx
@@ -9,6 +9,7 @@
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
+import React from 'react'
 
 export const dynamic = 'force-static'
 
---
+++ src/sanity/env.ts
@@ -0,0 +1,13 @@
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-04-28'
+
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage)
+  }
+
+  return v
+}
