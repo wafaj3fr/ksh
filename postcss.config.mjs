27fa974b572fd27ce4e6ffecbd029ee6c23912d7
Batman99SD <azozkrg1@gmail.com>
2025-05-07 13:41:09 +0300

    edit the schemas and the project data

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,6 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+const config = {
+  darkMode: 'class',
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
