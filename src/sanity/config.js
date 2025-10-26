56b7a270ce87b53a0ca32dd368ff23da9137d93b
razig17 <abdelrazig.sharif1@gmail.com>
2025-10-26 17:29:41 +0300

    Removed legacy config files

--- src/sanity/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    projectId: "your-project-id", // Replace with your actual project ID
-    dataset: "production",       // Replace with your dataset name (e.g., "production")
-    apiVersion: "2023-01-01",    // Use the latest API version
-    useCdn: true,                // Set to `false` if you need fresh data
-  };
-  
-  export default config;
